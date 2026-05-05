ba14fefec2c6da64e72bca683b17885447e36088
Allyson Carter <arcarter975@mail.cfcc.edu>
2026-05-05 16:11:57 -0400

    2nd push

--- search.php
+++ search.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
+<!--Allyson Carter-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="Styles/main.css">
+    <link rel="script" type="text/js" href="Scripts/main.js">
 </head>
 <body>
     
